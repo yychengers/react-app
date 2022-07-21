eea6c52054d2f80fb23e662d339b64bee48b69d7
yuanyancheng <617217327@qq.com>
2022-07-21 19:16:35 +0800

    feat: 新的更新

---
+++ ask.js
@@ -0,0 +1,23 @@
+module.exports = [
+    {
+        type: 'confirm',
+            name: 'private',
+            message: 'ths resgistery is private?',
+    },
+    {
+        type: 'input',
+        name: 'author',
+        message: 'author?',
+    }, 
+    {
+        type: 'input',
+        name: 'description',
+        message: 'description?',
+    }, 
+    {
+        type: 'input',
+        name: 'license',
+        message: 'license?',
+    }
+
+];
