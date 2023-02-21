4bb070358544cfc7fbe9a2c43b0ef3d4f1ad78ef
yuanyancheng <617217327@qq.com>
2023-02-22 07:47:25 +0800

    feat: add emotion/react

--- src/App.js
+++ src/App.js
@@ -4,12 +4,15 @@ import Test from './components/Test';
 import Demo1 from './components/Demo1';
 import Demo2 from './components/Demo2';
 
+import EmotionDemo from './components/EmotionDemo';
+
 function App() {
   return (
     <div className='App'>
       {/* <Test /> */}
       <Demo1 />
       <Demo2 />
+      <EmotionDemo />
     </div>
   );
 }
---
+++ src/components/EmotionDemo/index.jsx
@@ -0,0 +1,62 @@
+/** @jsxImportSource @emotion/react */
+import { css } from '@emotion/react';
+import styled from '@emotion/styled';
+
+const color = 'white';
+
+const Button = styled.button`
+  padding: 32px;
+  background-color: hotpink;
+  font-size: 24px;
+  border-radius: 4px;
+  color: black;
+  font-weight: bold;
+  &:hover {
+    color: white;
+  }
+`;
+
+const styles = css`
+  color: hotpink;
+`;
+const SomeComponent = ({ children }) => {
+  return (
+    <div css={styles}>
+      几个文字。。。
+      {children}
+    </div>
+  );
+};
+
+const AnotherStyles = css({
+  textDecoration: 'underline',
+});
+const AnotherComponent = () => {
+  return <div css={AnotherStyles}>一些文字，带下划线</div>;
+};
+
+const EmotionDemo = () => {
+  return (
+    <>
+      <div
+        css={css`
+          padding: 32px;
+          background-color: hotpink;
+          font-size: 24px;
+          border-radius: 4px;
+          &:hover {
+            color: ${color};
+          }
+        `}
+      >
+        hover me...
+        <Button>this is my button...</Button>
+      </div>
+      <SomeComponent>
+        <AnotherComponent></AnotherComponent>
+      </SomeComponent>
+    </>
+  );
+};
+
+export default EmotionDemo;
